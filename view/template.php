cf0a0b40aea77bbbc4e7d1a9d0e63c0045ff406d
PascalTed <monemail@email.com>
2018-11-15 10:27:46 +0100

    Initialisé code dans view/template.php

---
+++ view/template.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+    
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+        <link rel="icon" type="image/png" href="">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/style.css">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <meta name="description" content="">
+        
+        <!-- Facebook, Open Graph data -->
+		<meta property="og:title" content="">
+		<meta property="og:type" content="website">
+		<meta property="og:url" content="">
+		<meta property="og:image" content="">
+		<meta property="og:description" content="">
+        
+        <!-- Twitter Card data -->
+		<meta name="twitter:card" content="summary">
+		<meta name="twitter:site" content="">
+		<meta name="twitter:title" content="">
+		<meta name="twitter:description" content="">
+		<meta name="twitter:image" content="">
+    </head>
+    
+    <body>
+        <header>
+        </header>
+        
+        <footer>
+        </footer>
+    </body>
+
+</html>
