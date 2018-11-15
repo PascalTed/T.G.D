2a2f4c153bd9916ec6ac91b4a4cef315cbbb4808
PascalTed <monemail@email.com>
2018-11-15 11:18:00 +0100

    Initialisé code dans view/menuView.php

---
+++ view/menuView.php
@@ -0,0 +1,12 @@
+<?php ob_start(); ?>
+
+<nav id="header-nav">
+    <ul>
+        <li><a href="#">Accueil</a></li>
+        <li><a href="#">Nos jeux</a></li>
+        <li><a href="#">Bons plans</a></li>
+        <li><a href="#">Compte</a></li>
+    </ul>
+</nav>
+
+<?php $menu = ob_get_clean(); ?>
