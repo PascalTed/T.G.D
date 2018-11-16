e86bd143521033c0ab27c7f000f157fd338539b8
PascalTed <monemail@email.com>
2018-11-16 09:36:06 +0100

    Modifié texte dans view/menuView.php

--- view/menuView.php
+++ view/menuView.php
@@ -5,7 +5,7 @@
         <li><a href="#">Accueil</a></li>
         <li><a href="#">Nos jeux</a></li>
         <li><a href="#">Bons plans</a></li>
-        <li><a href="#">Compte</a></li>
+        <li><a href="#">Connexion</a></li>
     </ul>
 </nav>
 
