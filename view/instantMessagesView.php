f807678e979bb4e0c046a14313f8a9bddd69e42f
PascalTed <monemail@email.com>
2018-11-19 17:26:26 +0100

    Initialisé code dans view/instantMessagesView.php

---
+++ view/instantMessagesView.php
@@ -0,0 +1,41 @@
+<?php require_once('menuView.php'); ?>
+<?php ob_start(); ?>
+
+<div id="instant-messages">
+    <h3>messages</h3>
+    
+    <div>
+
+        <form action="index.php?action=addMessage" method="post" id="form-add-message">
+
+            <?php 
+            if (isset($_SESSION['pseudo'])) {
+            ?>
+                <label for="add-message">Laisser un message</label><br />
+                <textarea type="text" id="add-message" name="add-message" rows="10" cols="30"></textarea><br />
+                <span id="message-required"></span>
+                <input type="submit" value="Envoyer" />
+
+                <?php
+                } else {
+                ?>
+
+                <p id="no-connected">Connectez-vous pour laisser un message.</p>
+                <p><a href="#" id="se-connecter">Se connecter</a></p>
+
+                <?php
+                }
+                ?>
+
+        </form>
+        
+        <div id="all-messages">
+            
+        </div>
+        
+    </div>
+</div>
+
+<?php $messages = ob_get_clean(); ?>
+
+<?php require_once('template.php'); ?>
