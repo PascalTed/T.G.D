943a1da4abe2de2be18c442c8513c6421b487c3e
PascalTed <monemail@email.com>
2018-11-19 11:05:22 +0100

    Ajouté balise li (Forum) dans view/menuView.php

--- view/menuView.php
+++ view/menuView.php
@@ -3,6 +3,7 @@
 <nav id="header-menu">
     <ul>
         <li><a href="#">Accueil</a></li>
+        <li><a href="#">Forum</a></li>
         <li><a href="#">Nos jeux</a></li>
         <li><a href="#">Bons plans</a></li>
         <li><a href="#">Connexion</a></li>
