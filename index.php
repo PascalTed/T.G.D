9b8a1b70ecf1134311ec6b3c0cecd479d25390a1
PascalTed <monemail@email.com>
2018-11-20 15:33:20 +0100

    Supprimé accolade en trop dans index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once('controller/controller.php');
 try {
     if (isset($_GET['action'])) {
         // Vérification des informations saisies (pseudo et pass), venant d'un ajaxpost, avant de se connecter
-        } if ($_GET['action'] == 'connectAccount'){
+        if ($_GET['action'] == 'connectAccount'){
             verifPseudoPass($_POST['pseudo-connect'], $_POST['password-connect']);
         }
     } else {
