aabe29b564e8b68aa53da0b754861ddcc2c1d295
PascalTed <monemail@email.com>
2018-11-20 10:48:29 +0100

    Initialisé code dans view/toLoginView.php

---
+++ view/toLoginView.php
@@ -0,0 +1,28 @@
+<?php ob_start(); ?>               
+
+<div id="connexion">
+    <form action="#" method="post">
+        <i class="fas fa-times fa-2x" id="close-window-login"></i>
+        <p>
+            <label for="pseudo-connect">Pseudo : </label><br />
+            <input type="text" id="pseudo-connect" name="pseudo-connect" />
+        </p>
+        <p>
+            <label for="password-connect">Mot de passe : </label><br />
+            <input type="password" id="password-connect" name="password-connect" />
+        </p>
+        
+        <p id="pseudo-pass-alert"></p>
+        
+        <p>
+            <input type="submit" value="Se connecter"/>
+        </p>
+                
+        <p>
+            <a href="">Pas encore de compte ? Inscrivez-vous !</a>
+        </p>
+        
+    </form>
+</div>
+
+<?php $connexion = ob_get_clean(); ?>
