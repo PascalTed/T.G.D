e36c27f4f466daa57ecc19e5d921182577b2ae85
PascalTed <monemail@email.com>
2018-11-20 11:47:46 +0100

    Ajouté code affichage de la fenêtre de connexion dans js/appli.js

--- js/appli.js
+++ js/appli.js
@@ -22,3 +22,29 @@ mediaQuery.addListener(function(changed) {
     } 
 });
 // Fin menu burger
+
+// Début affichage de la fenêtre de connexion
+var loginWindow = document.getElementById("login-window");
+var opaqueWindow = document.getElementById("opaque-window");
+var menuConnect = document.getElementById("menu-connect");
+
+if (menuConnect !== null) {
+    
+    menuConnect.addEventListener("click", function() {
+        loginWindow.style.display = "block";
+        opaqueWindow.style.display = "block";
+                
+        opaqueWindow.addEventListener("click", function() {
+            opaqueWindow.style.display = "none";
+            loginWindow.style.display = "none";
+        });
+    }); 
+}
+
+var closeWindowLogin = document.getElementById("close-window-login");
+
+    closeWindowLogin.addEventListener("click", function() {
+        loginWindow.style.display = "none";
+        opaqueWindow.style.display = "none";
+    });
+// Fin affichage de la fenêtre de connexion
