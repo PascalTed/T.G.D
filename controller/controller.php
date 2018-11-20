2140afbdba3eb21198f364b96e5226b15aadeb69
PascalTed <monemail@email.com>
2018-11-20 14:46:33 +0100

    Ajouté require model/AccountManage.php et ajouté fonction verifPseudoPass() dans controller/controller.php

--- controller/controller.php
+++ controller/controller.php
@@ -1,12 +1,19 @@
 <?php
 
 // Chargement des classes
-
+require_once('model/AccountManage.php');
 
 // Affiche la liste des billets
 function demarrer()
 {
     require_once('view/instantMessagesView.php');
+}
+
+// Vérification des informations saisies (pseudo et pass), venant d'un ajaxpost, avant de se connecter
+function verifPseudoPass($pseudo, $pass) 
+{
+    $accountManager = new AccountManager();
+    $accountManager->searchPseudoPass($pseudo, $pass);
 }
 
 ?>
