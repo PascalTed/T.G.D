8f0505f3fc274304575ebef5ae7be5c890d897ec
PascalTed <monemail@email.com>
2018-11-20 14:41:08 +0100

    Ajouté condition connectAccount dans index.php

--- index.php
+++ index.php
@@ -2,9 +2,14 @@
 require_once('controller/controller.php');
 
 try {
-
-demarrer();
-
+    if (isset($_GET['action'])) {
+        // Vérification des informations saisies (pseudo et pass), venant d'un ajaxpost, avant de se connecter
+        } if ($_GET['action'] == 'connectAccount'){
+            verifPseudoPass($_POST['pseudo-connect'], $_POST['password-connect']);
+        }
+    } else {
+        demarrer();
+    }
 }
 catch(Exception $e) {
     echo 'Erreur : ' . $e->getMessage();
