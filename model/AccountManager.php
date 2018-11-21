4bf33e5a80654061d2408a9dd90eee587eade6e5
PascalTed <monemail@email.com>
2018-11-21 14:34:45 +0100

    Ajouté fonction searchPseudoMail() dans model/AccountManager.php

--- model/AccountManager.php
+++ model/AccountManager.php
@@ -5,6 +5,28 @@ require_once("model/Manager.php");
 class AccountManager extends Manager
 {
     
+     // Vérification des informations saisies (pseudo et email), venant d'un ajaxpost, avant de créer un compte.
+    public function searchPseudoMail($pseudo, $mail) 
+    {
+        $db = $this->dbConnect();
+        
+        $pseudoAccount = $db->prepare('SELECT id, pseudo, email FROM users WHERE pseudo = ?');
+        $pseudoAccount->execute(array($pseudo));
+        $existingUser = $pseudoAccount->fetch();
+        
+        $mailAccount = $db->prepare('SELECT id, pseudo, email FROM users WHERE email = ?');
+        $mailAccount->execute(array($mail));
+        $existingMail = $mailAccount->fetch();
+
+        if ($existingUser['pseudo']) {
+            echo "existUser";
+        } elseif ($existingMail['email']) {
+            echo "existEmail";
+        } else{
+            echo "valide";
+        } 
+    }
+    
     // Création du compte
     public function editAccount($pseudo, $mail, $pass)
     {
