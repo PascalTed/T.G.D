38b1d36d888a4744cd97ecfa41bbdaf91435635b
PascalTed <monemail@email.com>
2018-11-21 11:36:12 +0100

    Ajouté condition createAccount dans index.php

--- index.php
+++ index.php
@@ -6,8 +6,17 @@ require_once('controller/controller.php');
 
 try {
     if (isset($_GET['action'])) {
+        
+        // Enregistrement du nouveau compte
+        if ($_GET['action'] == 'createAccount') {
+            if (isset($_POST['pseudo']) && isset($_POST['email']) && isset($_POST['password'])) {
+                createAccount($_POST['pseudo'], $_POST['email'], $_POST['password']);
+            } else {
+                throw new Exception('Les informations (pseudo, email, password) n\'ont pas été envoyés.');
+            }
+            
         // Vérification des informations saisies (pseudo et pass), venant d'un ajaxpost, avant de se connecter
-        if ($_GET['action'] == 'connectAccount'){
+        } elseif ($_GET['action'] == 'connectAccount'){
             verifPseudoPass($_POST['pseudo-connect'], $_POST['password-connect']);
             
         // Déconnexion    
