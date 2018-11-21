396712cadee40391b19e17e856a36b85a723b815
PascalTed <monemail@email.com>
2018-11-21 14:11:46 +0100

    Ajouté lien href dans view/menuView.php

--- view/menuView.php
+++ view/menuView.php
@@ -2,7 +2,7 @@
 
 <nav id="header-menu">
     <ul>
-        <li><a href="#">Accueil</a></li>
+        <li><a href="index.php">Accueil</a></li>
         <li><a href="#">Forum</a></li>
         <li><a href="#">Nos jeux</a></li>
         <li><a href="#">Bons plans</a></li>
