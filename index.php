b41499bc862c68555342a137a28f4c674d79d79d
PascalTed <monemail@email.com>
2018-11-21 11:06:50 +0100

    Ajouté condition logoutAccount dans index.php

--- index.php
+++ index.php
@@ -9,6 +9,14 @@ try {
         // Vérification des informations saisies (pseudo et pass), venant d'un ajaxpost, avant de se connecter
         if ($_GET['action'] == 'connectAccount'){
             verifPseudoPass($_POST['pseudo-connect'], $_POST['password-connect']);
+            
+        // Déconnexion    
+        } elseif ($_GET['action'] == 'logoutAccount') {
+            if (isset($_SESSION['pseudo'])) {
+                logoutAccount();
+            } else {
+                throw new Exception('Pas de session d\'enregistrée, il faut être connecté pour se déconnecté.'); 
+            }
         }
     } else {
         demarrer();
