bf1fab60d8d7cc5bb4b27a92d1d0e268b00e5993
PascalTed <monemail@email.com>
2018-11-21 11:44:38 +0100

    Ajouté fonction editAccount() dans model/AccountManager.php

--- model/AccountManager.php
+++ model/AccountManager.php
@@ -4,6 +4,25 @@ require_once("model/Manager.php");
 
 class AccountManager extends Manager
 {
+    
+    // Création du compte
+    public function editAccount($pseudo, $mail, $pass)
+    {
+        $passHash = password_hash($pass, PASSWORD_DEFAULT);
+        $db = $this->dbConnect();
+        $account = $db->prepare('INSERT INTO users (pseudo, pass, email) VALUES (?, ?, ?)');
+        $account->execute(array($pseudo, $passHash, $mail));
+        
+        $sessionAccount = $db->prepare('SELECT id, avatar, user_right FROM users WHERE pseudo = ?');
+        $sessionAccount->execute(array($pseudo));
+        $infosSession = $sessionAccount->fetch();
+        
+        $_SESSION['id'] = $existingUsers['id'];
+        $_SESSION['avatar'] = $existingUsers['avatar'];
+        $_SESSION['user_right'] = $existingUsers['user_right'];
+        $_SESSION['pseudo'] = $pseudo;  
+    }
+    
     // Vérification des informations saisies (pseudo et pass), venant d'un ajaxpost, avant de se connecter
     public function searchPseudoPass($pseudo, $pass) 
     {
