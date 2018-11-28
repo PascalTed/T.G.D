f1a5b75a00d4e8b25c843e106452e358cf472db2
PascalTed <monemail@email.com>
2018-11-28 09:52:19 +0100

    Initialisé code dans model/MessageManager.php

---
+++ model/MessageManager.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once("model/Manager.php");
+
+class AccountManager extends Manager
+{
+    // Enregistrer le message
+    public function editMessage($userId, $message); 
+    {
+        $db = $this->dbConnect();
+        
+        $message = $db->prepare('INSERT INTO messages(user_id, message, message_date) VALUES(?, ?)');
+    }
+}
+
+?>
+        
