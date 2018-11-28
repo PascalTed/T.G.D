a806bf37e046305ca785a2aaaf3da0e377aa3c1e
PascalTed <monemail@email.com>
2018-11-28 10:31:58 +0100

    Modifié function editMessage() dans model/MessageManager.php

--- model/MessageManager.php
+++ model/MessageManager.php
@@ -5,12 +5,12 @@ require_once("model/Manager.php");
 class MessageManager extends Manager
 {
     // Enregistrer le message
-    public function editMessage($userId, $message)
+    public function editMessage($userId, $instantMessage)
     {
         $db = $this->dbConnect();
         
-        $message = $db->prepare('INSERT INTO messages(user_id, message) VALUES(?, ?)');
-        $message->execute(array($userId, $message));
+        $message = $db->prepare('INSERT INTO messages(user_id, message, message_date) VALUES(?, ?, NOW())');
+        $message->execute(array($userId, $instantMessage));
     }
 }
 
