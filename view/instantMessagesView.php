39dbffaa5fddeb945039a6b62c596e852ca3dbe0
PascalTed <monemail@email.com>
2018-11-29 14:34:19 +0100

    Modifié et nettoyé code dans view/instantMessagesView.php

--- view/instantMessagesView.php
+++ view/instantMessagesView.php
@@ -3,35 +3,46 @@
 <div id="instant-messages">
     <h3>messages</h3>
     
-    <div>
-
+    <div id="form-all-messages">
         <form action="index.php?action=addMessage" method="post" id="form-add-message">
 
             <?php 
             if (isset($_SESSION['pseudo'])) {
             ?>
+            
                 <label for="add-message">Laisser un message</label><br />
                 <textarea type="text" id="add-message" name="add-message" rows="10" cols="30"></textarea><br />
                 <span id="message-required"></span>
                 <input type="submit" value="Envoyer" />
-
-                <?php
-                } else {
-                ?>
-
+        </form>
+            
+            <?php
+            } else {
+            ?>
+        
                 <p id="no-connected">Connectez-vous pour laisser un message.</p>
                 <p><a href="#" id="messages-connect">Se connecter</a></p>
 
-                <?php
-                }
-                ?>
-
-        </form>
-        
+            <?php
+            }
+            ?>
         <div id="all-messages">
-            
-        </div>
         
+            <?php    
+            while ($comment = $req->fetch()) {
+            ?>
+                <div id="<?= $comment['id'] ?>" class="message">
+                    <!-- Affichage de chaque message (toutes les données sont protégées par htmlspecialchars -->
+                    <p><strong><?= htmlspecialchars($comment['pseudo']) ?></strong><em> le <?= $comment['message_date_fr'] ?></em></p>
+                
+                    <p>"<?= nl2br(htmlspecialchars($comment['message'])) ?>"</p>
+                </div>
+            <?php
+            }
+            $req->closeCursor();
+            ?>
+            
+         </div>
     </div>
 </div>
 
