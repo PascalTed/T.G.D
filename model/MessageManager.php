517798344ba9e6b485bd3b39e2c37c0b5815ea64
PascalTed <monemail@email.com>
2018-11-29 16:00:38 +0100

    Ajouté les fonctions getMessage() et getLastMessage() dans model/MessageManager.php

--- model/MessageManager.php
+++ model/MessageManager.php
@@ -12,6 +12,27 @@ class MessageManager extends Manager
         $message = $db->prepare('INSERT INTO messages(user_id, message, message_date) VALUES(?, ?, NOW())');
         $message->execute(array($userId, $instantMessage));
     }
+    
+    // Obtenir tous les messages
+    public function getMessage()
+    {
+        $db = $this->dbConnect();
+        
+        $messages = $db->query('SELECT messages.id, messages.user_id, messages.message, DATE_FORMAT(messages.message_date, \'%d/%m/%Y à %Hh%imin%ss\') AS message_date_fr, users.pseudo FROM messages INNER JOIN users ON messages.user_id = users.id ORDER BY messages.id DESC LIMIT 0, 25');
+
+        return $messages;
+    }
+    
+    // Obtenir les derniers messages
+    public function getLastMessage($messageId)
+    {
+        $db = $this->dbConnect();
+        
+        $messages = $db->prepare('SELECT messages.id, messages.user_id, messages.message, DATE_FORMAT(messages.message_date, \'%d/%m/%Y à %Hh%imin%ss\') AS message_date_fr, users.pseudo FROM messages INNER JOIN users ON messages.user_id = users.id AND messages.id > ? ORDER BY messages.id DESC');
+        $req->execute(array($messageId));
+        
+        return $messages;
+    }
 }
 
 ?>
