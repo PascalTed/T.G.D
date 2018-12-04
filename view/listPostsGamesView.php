c3fcb411ec498e2f6e6f0a421b3b3c0f8a837313
PascalTed <monemail@email.com>
2018-12-04 14:03:23 +0100

    Ajouté boucle while dans view/listPostsGamesView.php

--- view/listPostsGamesView.php
+++ view/listPostsGamesView.php
@@ -10,9 +10,35 @@
     </div>
 
     <div class="all-games-content">
-
+        
+        <?php    
+        while ($game = $games->fetch()) {
+            
+            $GameExtract = $game['content'];
+            $GameExtract = substr($GameExtract, 0, 200);
+            $spacePosition = strrpos($GameExtract, " ");
+            if ($spacePosition) {
+                $GameExtract = substr($GameExtract, 0, $spacePosition);
+            }
+        ?>
+            
+            <div class="game-content">
+                <h2><?= $game['title'] ?></h2>
+                
+                <img src="images/games/<?= $game['image'] ?>" class="image-game" alt="image du jeu"/> 
+                
+                <!-- Toutes les données sont protégées par htmlspecialchars -->
+                <p class="game-extract"><?= strip_tags($GameExtract) ?><a class="read-more-game" href="index.php?action=postGame&amp;idGame=<?= $game['id']; ?>"> <em>... lire la suite</em></a>
+                </p>
+                
+            </div>
+            
+        <?php
+        }
+        ?>
+        
     </div>
-
+    
 </section>
 
 <?php $content = ob_get_clean(); ?>
