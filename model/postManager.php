425917681c5d9d8ed002ac0314f355d0ba98c554
PascalTed <monemail@email.com>
2018-12-08 14:54:25 +0100

    Ajouté fonction getTopics() et modifié fonction getForums() dans model/postManager.php

--- model/postManager.php
+++ model/postManager.php
@@ -22,15 +22,22 @@ class PostManager extends Manager
         return $game;
     }
     
-       public function getForums()
-   {
+    public function getForums()
+    {
         $db = $this->dbConnect();
         
-        // liste des forums | nombre de sujets par forum | date des derniers messages avec pseudos
-        $forums = $db->query('SELECT id, categories, nb_topics, pseudo, last_date FROM (SELECT forums.id, forums.categories, COUNT(forums.categories) nb_topics FROM forums INNER JOIN topics ON forums.id = topics.forum_id GROUP BY forums.categories) table_a INNER JOIN (SELECT topics_messages.forum_id, MAX(topics_messages.message_date) last_date, users.pseudo FROM topics_messages INNER JOIN users ON topics_messages.user_id = users.id GROUP BY topics_messages.forum_id) table_b ON table_a.id = table_b.forum_id'); 
+        $forums = $db->query('SELECT id, categories, nb_topics, pseudo, last_date FROM (SELECT forums.id, forums.categories, COUNT(topics.forum_id) nb_topics FROM forums LEFT JOIN topics ON forums.id = topics.forum_id GROUP BY forums.categories) table_a LEFT JOIN (SELECT topics_messages.forum_id, MAX(topics_messages.message_date) last_date, users.pseudo FROM topics_messages INNER JOIN users ON topics_messages.user_id = users.id GROUP BY topics_messages.forum_id) table_b ON table_a.id = table_b.forum_id ORDER BY nb_topics DESC'); 
         
-        Return $forums;
+        return $forums;
+    }
+     
+    public function getTopics($forumId)
+    {
+        $db = $this->dbConnect();
         
+        $topics = $db->prepare('SELECT topicID, messageID, title, nb_message, t_pseudo, creation_date, tm_pseudo, last_date FROM (SELECT topics.id topicID, COUNT(topics_messages.topic_id) nb_message, topics.title, users.pseudo t_pseudo, topics.creation_date FROM topics INNER JOIN users ON users.id = topics.user_id INNER JOIN topics_messages ON topics_messages.topic_id = topics.id WHERE topics.forum_id = ? GROUP BY topicID) t1 INNER JOIN (SELECT topics_messages.id messageID, topics_messages.topic_id, users.pseudo tm_pseudo, MAX(topics_messages.message_date) last_date FROM topics_messages INNER JOIN users ON topics_messages.user_id = users.id GROUP BY topics_messages.topic_id) t2 ON t1.topicID = t2.topic_id'); 
+        $topics->execute(array($forumId));
+        
+        return $topics;
      }
-    
 }
