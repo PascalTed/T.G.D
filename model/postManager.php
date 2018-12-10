3e6f6e9e8174ffb6e4b7fb03c93c4021f4c79fb4
PascalTed <monemail@email.com>
2018-12-10 11:47:39 +0100

    Renommé fonction getlistPostsGames() en getListGames() dans model/postManager.php

--- model/postManager.php
+++ model/postManager.php
@@ -4,7 +4,7 @@ require_once("model/Manager.php");
 
 class PostManager extends Manager
 {
-    public function getlistPostsGames()
+    public function getListGames()
     {
         $db = $this->dbConnect();
         $games = $db->query('SELECT id, title, image, content FROM played_games ORDER BY creation_date DESC');
