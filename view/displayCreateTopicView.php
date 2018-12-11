59754b457b741b3fdc3a754e3ea0fa6dea98fc57
PascalTed <monemail@email.com>
2018-12-11 12:11:38 +0100

    Initialisé code dans view/displayCreateTopicView.php

---
+++ view/displayCreateTopicView.php
@@ -0,0 +1,30 @@
+<?php require_once('menuView.php'); ?>
+<?php require_once('toLoginView.php'); ?>
+
+<?php ob_start(); ?>
+
+<section id="create-topic">
+    
+    <div><a href="index.php?action=displayForumTopics&amp;idForum=<?= $forumIdCat['id'] ?>">Forum <?= $forumIdCat['categories'] ?></a>
+    </div>
+    
+    <div>
+        <form class="form-tiny-mce" action="index.php?action=editTopic&amp;idForum=<?= $forumIdCat['id'] ?>" method="post" id="form-create-topic">
+            <p>
+                <label for="create-title-topic">Ajouter le titre</label>
+                <textarea id="create-title-topic" name="create-title-topic"></textarea>
+                <span id="no-title"></span>
+
+                <label for="create-content-topic">Ajouter le contenu</label>
+                <textarea id="create-content-topic" name="create-content-topic"></textarea>
+                <span id="no-content"></span>
+                
+                <input type="submit" value="Ajouter le sujet" />
+            </p>
+         </form>
+    </div>
+</section>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
