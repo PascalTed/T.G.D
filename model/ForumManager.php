9355cd1d3008990dd4d78864f616bc229544160c
PascalTed <monemail@email.com>
2018-12-15 12:03:41 +0100

    Renommé class PostManager en ForumManager dans model/ForumManager.php

--- model/ForumManager.php
+++ model/ForumManager.php
@@ -2,26 +2,8 @@
 
 require_once("model/Manager.php");
 
-class PostManager extends Manager
+class ForumManager extends Manager
 {
-    public function getListGames()
-    {
-        $db = $this->dbConnect();
-        $games = $db->query('SELECT id, title, image, content FROM played_games ORDER BY creation_date DESC');
-        
-        return $games;
-    }
-    
-    public function getGame($gameId)
-    {
-        $db = $this->dbConnect();
-        $req = $db->prepare('SELECT id, title, image, content, type, release_date FROM played_games WHERE id = ? ORDER BY creation_date DESC');
-        $req->execute(array($gameId));
-        $game = $req->fetch();
-        
-        return $game;
-    }
-    
     public function getForums()
     {
         $db = $this->dbConnect();
