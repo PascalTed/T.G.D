4d6a2433a21ca57c8b12ce804c087a5eaef17ca1
PascalTed <monemail@email.com>
2018-12-15 11:53:30 +0100

    Initialisé code dans model/GameManager.php

---
+++ model/GameManager.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once("model/Manager.php");
+
+class GameManager extends Manager
+{
+    public function getListGames()
+    {
+        $db = $this->dbConnect();
+        $games = $db->query('SELECT id, title, image, content FROM played_games ORDER BY creation_date DESC');
+        
+        return $games;
+    }
+    
+    public function getGame($gameId)
+    {
+        $db = $this->dbConnect();
+        $req = $db->prepare('SELECT id, title, image, content, type, release_date FROM played_games WHERE id = ? ORDER BY creation_date DESC');
+        $req->execute(array($gameId));
+        $game = $req->fetch();
+        
+        return $game;
+    }
+}
