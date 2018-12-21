bdb8fc4d3e60fdb89cace6dfffb26d2838deda87
PascalTed <monemail@email.com>
2018-12-21 12:18:08 +0100

    Ajouté fonction editForumCat() dans model/adminForumManager.php

--- model/adminForumManager.php
+++ model/adminForumManager.php
@@ -4,7 +4,13 @@ require_once("model/Manager.php");
 
 class AdminForumManager extends Manager
 {
-
+    public function editForumCat($userID, $forumCat)
+    {
+        $db = $this->dbConnect();
+        
+        $req = $db->prepare('INSERT INTO forums (user_id, categories) VALUES (?, ?)');
+        $req->execute(array($userID, $forumCat));
+    }
 }
 
 ?>
