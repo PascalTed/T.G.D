8e9da2a6ef5b41e202a60789610a38db56670dcf
PascalTed <monemail@email.com>
2018-12-26 15:15:24 +0100

    Modifié section (id admin-forums) dans view/backend/adminForumsView.php

--- view/backend/adminForumsView.php
+++ view/backend/adminForumsView.php
@@ -15,27 +15,12 @@
         while ($forum = $forums->fetch()) {
         ?>
         
-            <div class="admin-edit-forum">
-                <div class="admin-forum-cat-topics">
-                    <h2><a href="index.php?action=displayAdminForumTopics&amp;idForum=<?= $forum['id'] ?>&amp;catForum=<?= $forum['categories'] ?>"><?= $forum['categories'] ?> (<?= $forum['nb_topics'] ?> sujets)</a>
-                    </h2>
-                </div>
-                        
-                <form action="index.php?action=modifyOrRemoveForum&amp;idForum=<?= $forum['id'] ?>" class="form-edit-forum" method="post">
-                    <label>Modifier le nom du forum<br />
-                        <textarea class="textarea-cat-forum" name="textarea-cat-forum"><?= strip_tags($forum['categories']); ?></textarea>
-                    </label>
-                    <span class="forum-exist">Ce forum existe déjà.</span>
-                    <div class="forum-radio">
-                        <label><input type="radio" name="setForum" value="modify-forum-cat" class="adm-modify-forum" checked />Modifier le nom du forum</label>
-            
-                        <label><input type="radio" name="setForum" value="remove-forum-cat" class="adm-remove-forum" />Supprimer le forum</label><br />
-                    </div>
-            
-                    <input type="submit" value="Envoyer" />
-                </form>
-             </div>
-        
+            <div class="admin-forum-cat-topics">
+                <h2>
+                    <a href="index.php?action=displayAdminForumTopics&amp;idForum=<?= $forum['id'] ?>&amp;catForum=<?= $forum['categories'] ?>"><?= $forum['categories'] ?> (<?= $forum['nb_topics'] ?> sujets)</a>
+                </h2>
+            </div>
+    
         <?php
         }
         ?>
@@ -43,9 +28,8 @@
         <div id="add-forum-content">
             <form action="index.php?action=addForumCat" method="post" id="form-add-forum">
                 <label for="add-forum">Ajouter un forum</label><br />
-                <textarea type="text" id="add-forum" name="add-forum"></textarea>
+                <textarea type="text" id="add-forum" name="add-forum" required></textarea>
                 <span id="forum-exist">Ce forum existe déjà.</span>
-                <span id="forum-required">Le champ message n'est pas rempli.</span>
                 <input type="submit" value="Envoyer" />
             </form>
         </div>
