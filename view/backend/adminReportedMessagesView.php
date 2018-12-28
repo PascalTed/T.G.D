92d18f4afa6c00c6a71cdac3ed8787a4382b8fa1
PascalTed <monemail@email.com>
2018-12-28 14:09:00 +0100

    Modifié boucle while dans view/backend/adminReportedMessagesView.php

--- view/backend/adminReportedMessagesView.php
+++ view/backend/adminReportedMessagesView.php
@@ -32,7 +32,7 @@
 
                     <p>Message : <br /><?= $reportedMessage['tm_message'] ?></p>
 
-                    <p><a href="index.php?action=validMessage&amp;idMessage=<?= $reportedMessage['tm_id'] ?>">Valider</a><a href="index.php?action=removeMessaget&amp;idMessage=<?= $reportedMessage['tm_id'] ?>">Supprimer</a>
+                    <p><a href="index.php?action=validMessage&amp;idMessage=<?= $reportedMessage['tm_id'] ?>&amp;idTopic=<?= $reportedMessage['topicID'] ?>">Valider</a><a href="index.php?action=removeMessaget&amp;idMessage=<?= $reportedMessage['tm_id'] ?>">Supprimer</a>
                     </p>
             </div>
         
