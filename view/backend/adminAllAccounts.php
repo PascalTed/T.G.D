db9a358dabb1a6b06eee69e457567a1ece3a00f1
PascalTed <monemail@email.com>
2018-12-29 11:02:43 +0100

    Initialisé code dans view/backend/adminAllAccounts.php

---
+++ view/backend/adminAllAccounts.php
@@ -0,0 +1,34 @@
+<?php require_once('view/backend/adminMenuView.php'); ?>
+<?php require_once('view/frontend/toLoginView.php'); ?>
+
+<?php ob_start(); ?>
+
+<section id="admin-all-accounts">
+    
+    <div>
+        <h1 id="">Liste des utilisateurs</h1>
+    </div>
+
+    <div id="admin-all-accounts-content">
+        
+        <?php
+        while ($Account = $allAccounts->fetch()) {
+        ?>
+        
+            <div>
+                <p><a href="index.php?action=displayAdminAccount&amp;pseudo=<?= $Account['id'] ?>"><?= $Account['pseudo'] ?></a></p>
+                <p>Inscrit le : <?= $Account['registration_date'] ?></p>
+                <p>droits : <?= $Account['user_right'] ?></p>
+            </div>
+        
+        <?php
+        }
+        ?>
+        
+    </div>
+    
+</section>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('view/frontend/template.php'); ?>
