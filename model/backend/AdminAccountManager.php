0ffc798243f92c81c6c7742d4dc19fc8a9996fc2
PascalTed <monemail@email.com>
2018-12-29 12:18:52 +0100

    Modifé fonction getInfosAccount() dans model/backend/AdminAccountManager.php

--- model/backend/AdminAccountManager.php
+++ model/backend/AdminAccountManager.php
@@ -16,9 +16,10 @@ class AdminAccountManager extends Manager
     public function getInfosAccount($userId)
     {
         $db = $this->dbConnect();
-        $infosAccount = $db->prepare('SELECT id, email, avatar, pseudo, user_right, DATE_FORMAT(registration_date, \'%d/%m/%Y à %Hh%imin%ss\') registration_date FROM users WHERE id = ?');
-        $infosAccount->execute(array($userId));
-        
+        $req = $db->prepare('SELECT id, email, avatar, pseudo, user_right, DATE_FORMAT(registration_date, \'%d/%m/%Y à %Hh%imin%ss\') registration_date FROM users WHERE id = ?');
+        $req->execute(array($userId));
+        $infosAccount = $req->fetch();
+   
         return $infosAccount;
     }
 }
