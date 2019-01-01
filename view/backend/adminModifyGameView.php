4e9d7e4ceebf0a14fb92d39c8a32206d528432ce
PascalTed <monemail@email.com>
2019-01-01 16:23:21 +0100

    Renommé section (id admin-game en admin-edit-game) dans view/backend/adminModifyGameView.php

--- view/backend/adminModifyGameView.php
+++ view/backend/adminModifyGameView.php
@@ -3,7 +3,7 @@
 
 <?php ob_start(); ?>
 
-<section id="admin-game">
+<section id="admin-edit-game">
 
     <div>
         <h1>Détails du jeu</h1>
@@ -21,13 +21,13 @@
         <form action="index.php?action=modifyOrRemoveGame&amp;idGame=<?= $game['id'] ?>" id="form-edit-game" method="post">
             <p>
                 <label for="edit-title-game">Modifier le titre</label><br />
-                <textarea id="edit-title-game" name="edit-title-game"><?= $game['title']); ?></textarea>
+                <textarea id="edit-title-game" name="edit-title-game"><?= $game['title']; ?></textarea><br />
                 <span id="no-edit-title-game" class="messages-edit-game">Le champ titre est vide.</span>
             </p>
 
             <p>
                 <label for="edit-content-game">Modifier le contenu</label><br />
-                <textarea id="edit-content-game" name="edit-content-game"><?= $game['content']); ?></textarea>
+                <textarea id="edit-content-game" name="edit-content-game"><?= $game['content']; ?></textarea><br />
                 <span id="no-edit-content-game" class="messages-edit-game">Le champ contenu est vide.</span>
             </p>
 
@@ -41,12 +41,12 @@
 
             <p>
                 <label for="edit-type-game">Modifier le genre</label><br />
-                <textarea id="edit-type-game" name="edit-type-game"><?= $game['type']); ?></textarea>
+                <textarea id="edit-type-game" name="edit-type-game"><?= $game['type']; ?></textarea><br />
                 <span id="no-edit-type-game" class="messages-edit-game">Le champ genre est vide.</span>
             </p>
             <p>
                 <label for="edit-date-game">Modifier la date de sortie</label><br />
-                <textarea id="edit-date-game" name="edit-date-game"><?= $game['release_date']); ?></textarea>
+                <textarea id="edit-date-game" name="edit-date-game"><?= $game['release_date']; ?></textarea><br />
                 <span id="no-edit-date-game" class="messages-edit-game">Le champ genre est vide.</span>
             </p>
 
@@ -56,6 +56,7 @@
 
                 <label for="adm-remove-game">Supprimer le sujet</label>
                 <input type="radio" name="setGame" value="adm-remove-game" id="adm-remove-game" /><br />
+                <input type="submit" value="Envoyer" />
             </div>
         </form>
 
