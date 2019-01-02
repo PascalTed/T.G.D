ec0f982908a6ae5e8c4dfb8dcfb6106be3f75a84
PascalTed <monemail@email.com>
2019-01-02 15:16:40 +0100

    Modifié section (id admin-edit-game) dans view/backend/adminModifyGameView.php

--- view/backend/adminModifyGameView.php
+++ view/backend/adminModifyGameView.php
@@ -24,16 +24,22 @@
                 <textarea id="edit-title-game" name="edit-title-game"><?= $game['title']; ?></textarea><br />
                 <span id="no-edit-title-game" class="messages-edit-game">Le champ titre est vide.</span>
             </p>
-
+            
             <p>
-                <label for="edit-content-game">Modifier le contenu</label><br />
-                <textarea id="edit-content-game" name="edit-content-game"><?= $game['content']; ?></textarea><br />
-                <span id="no-edit-content-game" class="messages-edit-game">Le champ contenu est vide.</span>
+                <label for="edit-date-game">Modifier la date de sortie</label><br />
+                <textarea id="edit-date-game" name="edit-date-game"><?= $game['release_date']; ?></textarea><br />
+                <span id="no-edit-date-game" class="messages-edit-game">Le champ genre est vide.</span>
             </p>
-
+            
             <p>
-                <img src="images/games/<?= $game['image'] ?>" class="image-edit-game" alt="image du jeu à modifier"/>
+                <label for="edit-type-game">Modifier le genre</label><br />
+                <textarea id="edit-type-game" name="edit-type-game"><?= $game['type']; ?></textarea><br />
+                <span id="no-edit-type-game" class="messages-edit-game">Le champ genre est vide.</span>
+            </p>
+            
+            <p>
                 <label for="edit-file-game">Modifier l'image</label><br />
+                <img src="images/games/<?= $game['image'] ?>" class="image-edit-game" alt="image du jeu à modifier"/><br />
                 <input type="file" name="edit-file-game" id="edit-file-game" /><br />
                 <span id="accepted-edit-file-game">Fichiers acceptés : jpeg ou png, maximum 2Mo.</span><br />
                 <span id="max-edit-file-game" class="messages-edit-game">Le fichier est trop gros.</span>
@@ -41,14 +47,9 @@
             </p>
 
             <p>
-                <label for="edit-type-game">Modifier le genre</label><br />
-                <textarea id="edit-type-game" name="edit-type-game"><?= $game['type']; ?></textarea><br />
-                <span id="no-edit-type-game" class="messages-edit-game">Le champ genre est vide.</span>
-            </p>
-            <p>
-                <label for="edit-date-game">Modifier la date de sortie</label><br />
-                <textarea id="edit-date-game" name="edit-date-game"><?= $game['release_date']; ?></textarea><br />
-                <span id="no-edit-date-game" class="messages-edit-game">Le champ genre est vide.</span>
+                <label for="edit-content-game">Modifier le contenu</label><br />
+                <textarea id="edit-content-game" name="edit-content-game"><?= $game['content']; ?></textarea><br />
+                <span id="no-edit-content-game" class="messages-edit-game">Le champ contenu est vide.</span>
             </p>
 
             <div id="game-radio">
